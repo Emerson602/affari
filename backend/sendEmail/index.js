6fa67fee3840c60e087a6173e591e1bb3290ae77
Emerson602 <emerson.nic@hotmail.com>
2024-08-17 14:05:58 -0300

    update

--- backend/sendEmail/index.js
+++ backend/sendEmail/index.js
@@ -65,7 +65,7 @@ app.post('/send-email', (req, res) => {
     });
   });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
